623ce740fb3b8f96e78ee7fe81d207280dcb2ec0
tsvibt <tsvibt@gmail.com>
2015-04-29 23:15:04 -0500

    start moves

--- Beard.elm
+++ Beard.elm
@@ -2,31 +2,16 @@ module Beard
   ( ..
   ) where
 
-{- for now, assume the display tree
-is the same as tree that represents the semantics
-of which objects descend from which for purposes
-of moving whole subtrees around, folding whole subtrees
-away, etc. But these might be separated if the user
-wants to move a box around on the screen without
-changing its parent and children. -}
 import Object exposing (Object) 
 import LinearOrder exposing (LinearOrder)
 import Dict exposing (Dict)
 import Debug
 
-{- i changed my mind back to beards
-being just a single DisplayTree. this 
-avoids duplicating code and it provides a 
-neat solution to rooms with multiple roots:
-that room corresponds to a single db object
-that displays invisibly and has as children
-the would-be roots. still have special functions
-so that you don't generally add root nodes to beards.-}
 type alias Beard =
   { freshID : NodeID
-  , treeBeard : DisplayTree --leave this, or the Ulairi will get you
---  , currentFocus : NodeID --or Location?
+  , treeBeard : DisplayTree 
   }
+--  , currentFocus : NodeID --or Location?
 
 type alias DisplayTree =
   { nodeData : NodeData
@@ -37,18 +22,6 @@ type alias NodeData =
   { value : Object.ObjectInContext
   , location : Location
   }
- 
-idFromData : NodeData -> NodeID
-idFromData nodeData =
-  case nodeData.location |> List.reverse |> List.head of
-    Nothing -> Debug.crash "Beard.idFromData: node has no location"
-    Just (id,_) -> id
-
-kindFromData : NodeData -> NodeKind
-kindFromData nodeData =
-  case nodeData.location |> List.reverse |> List.head of
-    Nothing -> Debug.crash "Beard.kindFromData: node has no location"
-    Just (_,kind) -> kind
 
 type NodeKind = UpNode | DownNode | OverNode --more
 
@@ -79,9 +52,34 @@ singleton obj =
   let nodeData =
         { value = obj, location = [] } 
   in
-  { freshID = 1
-  , treeBeard = singletonTree nodeData 
-  } 
+  { freshID = 0
+  , treeBeard = singletonTree nodeData } 
+
+insert : Object.ObjectInContext -> Location ->
+         NodeKind -> Beard -> (Location,Beard)
+insert obj loc kind beard =
+  let id = beard.freshID 
+      loc' = loc ++ [(id,kind)]
+      newTree = singletonTree { value = obj, location = loc'}
+      newTreeBeard = treeInsert newTree loc' beard.treeBeard 
+  in 
+  ( loc', { freshID = id + 1, treeBeard = newTreeBeard } )
+
+{-
+delete : Location -> Beard -> Beard
+delete loc beard =
+  { beard | treeBeard <- treeDelete loc beard.treeBeard }
+
+move : Location -> Location -> Beard -> Beard
+move oldLoc newLoc beard =
+  { beard | treeBeard <- treeMove oldLoc newLoc beard.treeBeard }
+
+treeMove : Location -> Location -> DisplayTree
+treeMove oldLoc newLoc tree =
+  let (beard',tree) = treeDelete oldLoc beard
+  in 
+  insert
+-}
 
 singletonTree : NodeData -> DisplayTree
 singletonTree data =
@@ -102,74 +100,63 @@ emptyForest =
     , order = LinearOrder.empty
     }  
 
-{- this is not great because the type Children
-and the type NodeKind are not coupled at all.
-what is the right way to do this without writing a
-new insert function for each kind of descendant 
-we may want to add?-}
-treeInsert : NodeData -> Location
-             -> DisplayTree -> DisplayTree
-treeInsert nodeData loc tree =
-  case loc of
-    [] -> Debug.crash "Beard.treeInsert: can't insert at root" 
-    (id,kind)::loc' ->
-      let new = forestInsertRec nodeData id loc'  
-          children = tree.children
-          newchildren =
-           case kind of
+treeInsert : DisplayTree -> Location -> DisplayTree -> DisplayTree
+treeInsert newTree loc oldTree = case loc of
+  [] -> Debug.crash "Beard.treeInsert: can't insert at root" 
+  (id,kind)::loc' ->
+    let new = forestInsertRec newTree id kind loc'  
+        children = oldTree.children
+        newchildren =
+          case kind of
             UpNode ->
-              { children
-              | upNodes <- new children.upNodes }
+              { children | upNodes <- new children.upNodes }
             DownNode ->
-              { children
-              | downNodes <- new children.downNodes }
+              { children | downNodes <- new children.downNodes }
             OverNode ->
-              { children
-              | overNodes <- new children.overNodes }
-      in 
-      { tree | children <- newchildren }
+              { children | overNodes <- new children.overNodes }
+   in 
+   { oldTree | children <- newchildren }
 
-forestInsertRec : NodeData -> NodeID 
-                  -> Location -> Forest -> Forest 
-forestInsertRec nodeData id loc (Forest forest) =
+forestInsertRec : DisplayTree -> NodeID -> NodeKind -> Location -> Forest -> Forest 
+forestInsertRec newTree id kind loc (Forest forest) =
   case loc of 
-    [] -> forestInsertNew nodeData id (Forest forest)
-    _ -> 
-      case Dict.get id forest.trees of
-        Nothing ->
+    [] -> forestInsertNew newTree id kind (Forest forest)
+    _ -> case Dict.get id forest.trees of
+       Nothing ->
          Debug.crash "Beard.forestInsertRec: no such location"
-        Just tree -> 
-          let newTree = treeInsert nodeData loc tree
-              newTrees = 
-                Dict.update id (\_ -> Just newTree) forest.trees
-          in
-          Forest { forest | trees <- newTrees }
+       Just tree -> 
+         let updatedTree = treeInsert newTree loc tree
+             newTrees = Dict.update id (\_ -> Just updatedTree) forest.trees
+         in
+         Forest { forest | trees <- newTrees }
    
-forestInsertNew : NodeData -> NodeID -> Forest -> Forest
-forestInsertNew nodeData id (Forest forest) =
+forestInsertNew : DisplayTree -> NodeID -> NodeKind -> Forest -> Forest
+forestInsertNew newTree id kind (Forest forest) =
   if Dict.member id forest.trees 
   then Debug.crash "Bearh.forestInsertNew: id already exists"
   else 
-    let newTree = singletonTree nodeData
-        newTrees = Dict.insert id newTree forest.trees
-        newOrder = orderInsert nodeData id forest.order
+    let newTrees = Dict.insert id newTree forest.trees
+        newOrder = orderInsert id kind forest.order
     in
     Forest { trees = newTrees, order = newOrder }
 
-orderInsert : NodeData -> NodeID  -> Order -> Order 
-orderInsert nodeData id order =  
-  case kindFromData nodeData of
+orderInsert : NodeID -> NodeKind -> Order -> Order 
+orderInsert id kind order =  
+  case kind of
     DownNode -> LinearOrder.insertTop id order
     UpNode -> LinearOrder.insertBottom id order
     OverNode -> LinearOrder.insertBottom id order
+ 
+idFromData : NodeData -> NodeID
+idFromData nodeData =
+  case nodeData.location |> List.reverse |> List.head of
+    Nothing -> Debug.crash "Beard.idFromData: node has no location"
+    Just (id,_) -> id
 
-{-
-insert' : Object.ObjectInContext -> Location
-          -> Beard -> Beard
-insert' obj loc beard =
-  case loc of
-    [] -> let id = beard.freshID
+kindFromData : NodeData -> NodeKind
+kindFromData nodeData =
+  case nodeData.location |> List.reverse |> List.head of
+    Nothing -> Debug.crash "Beard.kindFromData: node has no location"
+    Just (_,kind) -> kind
 
-          in { }
--}
 
